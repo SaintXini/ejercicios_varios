412137a41b78e2124f7e44a5547b3335435d18e3
Saint <ss.mscx@gmail.com>
2026-07-08 16:40:50 -0600

    cambios luego de mucho tiempo

--- generator_password.py
+++ generator_password.py
@@ -15,6 +15,22 @@ def generate_password(min_Length, numbers = True, special_characters = True):
     pwd = ""
     meet_criteria = False
     has_number = False
+    has_special = False
+
+    while not meet_criteria or len(pwd) < min_Length:
+        new_char = random.choice(characters)
+        pwd += new_char
+
+        if new_char in digits:
+            has_number = True
+        elif new_char in special:
+            has_special = True
+
+        meets_criteria = True
+        if numbers:
+            meets_criteria = has_number
+        if special_characters:
+            meets_criteria = meets_criteria and has_special
 
 
 
